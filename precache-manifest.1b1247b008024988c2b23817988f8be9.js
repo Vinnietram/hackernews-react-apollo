5ee624e50eb9edea18818a2baaf79aba618589f7
DESKTOP-5S0VM6C\User <vinnietram@gmail.com>
2020-05-19 11:33:11 +0100

    Updates

---
+++ precache-manifest.1b1247b008024988c2b23817988f8be9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb42ed55af1215e3ddab9d58d6d07c8",
+    "url": "/hackernews-react-apollo/index.html"
+  },
+  {
+    "revision": "aaeb443b440024627ad3",
+    "url": "/hackernews-react-apollo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e79701084c913df828c3",
+    "url": "/hackernews-react-apollo/static/js/2.78c5fc11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackernews-react-apollo/static/js/2.78c5fc11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaeb443b440024627ad3",
+    "url": "/hackernews-react-apollo/static/js/main.0fb80682.chunk.js"
+  },
+  {
+    "revision": "552188f0a62680e3530e",
+    "url": "/hackernews-react-apollo/static/js/runtime-main.eab5cbb1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackernews-react-apollo/static/media/logo.5d5d9eef.svg"
+  }
+]);
